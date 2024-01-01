538d5701ef306666e2b6810e5bf1fba489f40ce8
Anmol Arora <anmolarora507@gmail.com>
2024-01-01 14:00:14 +0530

    video-2 React app component and jsx

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ['Bob', 'Kevin', 'Dave'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to see changes.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Hello {handleNameChange()}!</p>
+        {/* curly braces represent jsx expressions. JSX expression convert everything into text. Without curly braces it takes it as a normal html */}
+        {/* Obects and boolean are not rendered on the page. They will throw some error. Eg. {a: 2, b: 3} and {1 === 2} will throw error */}
+        <p>html {"Dave"} 1 {[1, 2, 3]} [1, 2, 3]</p>
       </header>
     </div>
   );
