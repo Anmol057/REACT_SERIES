e3500849a92217ab0d999f2c293f9fc37b4d3bc0
Anmol Arora <anmolarora507@gmail.com>
2024-01-02 17:05:38 +0530

    video 3 functional components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  const handleNameChange = () => {
-    const names = ['Bob', 'Kevin', 'Dave'];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> Hello {handleNameChange()}!</p>
-        {/* curly braces represent jsx expressions. JSX expression convert everything into text. Without curly braces it takes it as a normal html */}
-        {/* Obects and boolean are not rendered on the page. They will throw some error. Eg. {a: 2, b: 3} and {1 === 2} will throw error */}
-        <p>html {"Dave"} 1 {[1, 2, 3]} [1, 2, 3]</p>
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
